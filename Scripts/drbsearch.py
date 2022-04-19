1e80f19203b66975ac3bc6ef50fd312ad878b4bc
JosephParampathu <88171669+JosephParampathu@users.noreply.github.com>
2022-04-19 07:38:43 -0700

    Update drbsearch.py

--- Scripts/drbsearch.py
+++ Scripts/drbsearch.py
@@ -10,8 +10,10 @@ onlyfiles = [filename[0:filename.index(".")] for filename in listdir(mypath)
                 if isfile(join(mypath, filename))]
 
 
-#Check each file for the word "grant", return 1 or 0
+#Check each file for the strings "grant", "deny", and "liberal consideration" return 1 or 0
 onlygrants = []
+onlydenies = []
+onlymentalhealth = []
 
 for filename in listdir(mypath):
     myfile = open(mypath+"/"+filename)
@@ -19,24 +21,10 @@ for filename in listdir(mypath):
         onlygrants.append(1)
     else:
         onlygrants.append(0)
-    myfile.close()
-
-#Check each file for the word "deny", return 1 or 0
-onlydenies = []
-
-for filename in listdir(mypath):
-    myfile = open(mypath+"/"+filename)
     if('deny' in myfile.read()):
         onlydenies.append(1)
     else:
         onlydenies.append(0)
-    myfile.close()
-
-#Check each file for the string "liberal consideration", return 1 or 0
-onlymentalhealth = []
-
-for filename in listdir(mypath):
-    myfile = open(mypath+"/"+filename)
     if('liberal consideration' in myfile.read()):
         onlymentalhealth.append(1)
     else:
